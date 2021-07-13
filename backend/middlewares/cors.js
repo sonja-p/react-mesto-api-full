fa5c77a70edf55e947f8ccc71defb8c6b87c4158
Sofia Puolakainen <sonja.puolaka@yandex.ru>
2021-07-13 14:17:36 +0300

    rewrote cors middleware

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,25 +1,24 @@
 const allowedCors = [
   'https://puolukka.nomoredomains.rocks',
   'http://puolukka.nomoredomains.rocks',
-  'localhost:3000',
+  'http://localhost:3000',
 ];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
+    const { method } = req;
+
+    const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+    const requestHeaders = req.headers['access-control-request-headers'];
+    // Если это предварительный запрос, добавляем нужные заголовки
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+    }
+
     res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  const { method } = req;
-
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
   }
 
   next();
